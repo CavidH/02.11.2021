74326d45902860efcbcef84f560d2fbccc52728f
CavidH <hcavid386@gmail.com>
2021-11-02 10:26:02 +0400

    main

--- 02.11.2021/Program.cs
+++ 02.11.2021/Program.cs
@@ -14,14 +14,14 @@ namespace _02._11._2021
             Console.WriteLine(Join("-",s));
         }
 
-        public static string Join(string Sep, string[] Str)
+        public static string Join(string Sep, string[] Value)
         {
             StringBuilder stringBuilder = new StringBuilder();
-            for (int i = 0; i < Str.Length-1; i++)
+            for (int i = 0; i < Value.Length-1; i++)
             {
-                stringBuilder.Append(Str[i] + Sep);
+                stringBuilder.Append(Value[i] + Sep);
             }
-            stringBuilder.Append(Str[Str.Length-1]);
+            stringBuilder.Append(Value[Value.Length-1]);
             return stringBuilder.ToString();
         }
     }
