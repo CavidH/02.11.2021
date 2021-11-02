6e9aaa7cf214341894ae3696bf03975fed648cff
CavidH <hcavid386@gmail.com>
2021-11-02 11:20:07 +0400

    task 2

--- 02.11.2021/Program.cs
+++ 02.11.2021/Program.cs
@@ -9,9 +9,50 @@ namespace _02._11._2021
     {
         static void Main(string[] args)
         {
-            string[] s = { "salam", "necesen", "mende", "hello", "yazsiyam " };
-            Console.WriteLine(string.Join("-", s));
-            Console.WriteLine(Join("-",s));
+            string s = "sdfsm sdf";
+            foreach (var item in Split(' ',s))
+            {
+                Console.WriteLine(item);
+            }
+
+            #region join
+            string[] Input = { "salam", "necesen", "mende", "hello", "yazsiyam " };
+            Console.WriteLine(string.Join("-", Input));
+            Console.WriteLine(Join("-", Input));
+            #endregion
+
+        }
+        public static string[] Split(char Separator,string Value )
+        {
+            int count=1;
+            int count1 = 0;
+            for (int i = 0; i < Value.Length; i++)
+            {
+                if (Value[i]==Separator)
+                { 
+                    count++;
+                }
+            }
+            StringBuilder stringBuilder = new StringBuilder();
+            string[] Result = new string[count];
+            for (int i = 0; i < Value.Length; i++)
+            {
+                if (Value[i]!=Separator)
+                {
+                    stringBuilder.Append(Value[i]);
+                }
+
+                if (Value[i] == Separator || i==Value[Value.Length-1])
+                {
+                    Result[count1] = stringBuilder.ToString();
+                    stringBuilder.Clear();
+                    count1++;
+                }
+            }
+
+
+            return Result;
+            
         }
 
         public static string Join(string Sep, string[] Value)
